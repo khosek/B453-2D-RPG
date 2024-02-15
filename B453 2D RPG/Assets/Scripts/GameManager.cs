1b36634a5a025e9c950efc715877cbe3e0018e30
khosek <khosek@iu.edu>
2024-02-15 18:53:17 -0500

    Adds main menu w/ new game and end game buttons

---
+++ B453 2D RPG/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
